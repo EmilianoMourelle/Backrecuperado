2ecc777c013ad2605f721766e1a47088f56c0352
Elflacomou <elflacomurel@hotmail.com>
2023-05-10 23:32:49 -0300

    Primer comit despues del recupero Nuevo repo

--- src/main/java/com/portfoliomou/Mourelle/controlador/PersonaControlador.java
+++ src/main/java/com/portfoliomou/Mourelle/controlador/PersonaControlador.java
@@ -5,6 +5,7 @@ import com.portfoliomou.Mourelle.entidad.Persona;
 import com.portfoliomou.Mourelle.interfaz.IPersonaServicio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@CrossOrigin (origins = "http://localhost:4200")
+
 public class PersonaControlador {
     @Autowired IPersonaServicio ipersonaServicio;
     
@@ -31,5 +35,10 @@ public class PersonaControlador {
     public String deletePersona (@PathVariable Long id){
         ipersonaServicio.deletePersona(id);
         return "El usuario fue eliminado";
+    }
+    
+    @GetMapping ("/personas/buscar/{id}")
+    public Persona findPersona(@PathVariable Long id) {
+        return ipersonaServicio.findPersona(id);
     }
 }
